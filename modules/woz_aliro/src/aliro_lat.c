9d253c1ad26169789a43c781790244abab518bff
ace <asxeem@pm.me>
2026-07-22 09:20:40 +0530

    style: clang-format the latency-phase name table

--- modules/woz_aliro/src/aliro_lat.c
+++ modules/woz_aliro/src/aliro_lat.c
@@ -22,8 +22,8 @@
 static int64_t s_stamp_us[ALIRO_LAT_PHASE_COUNT];
 
 static const char *const k_phase_name[] = {
-	"connect", "spsm", "ver",  "l2cap", "op05", "auth0", "a0rsp",   "auth1", "exch", "apc",
-	"irs",     "m1",   "m2",   "m3",    "m4rx", "m4",    "range",   "trusted", "near", "bolt",
+	"connect", "spsm", "ver", "l2cap", "op05", "auth0", "a0rsp", "auth1",   "exch", "apc",
+	"irs",     "m1",   "m2",  "m3",    "m4rx", "m4",    "range", "trusted", "near", "bolt",
 };
 _Static_assert(sizeof(k_phase_name) / sizeof(k_phase_name[0]) == ALIRO_LAT_PHASE_COUNT,
 	       "k_phase_name must cover every aliro_lat_phase");
